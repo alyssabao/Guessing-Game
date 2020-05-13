772946b62e128d8c22c29469d076a6ad63229c0c
alyssabao <64719777+alyssabao@users.noreply.github.com>
2020-05-13 19:39:50 +0700

    completed basics

---
+++ main.js
@@ -0,0 +1,35 @@
+let randomNum = Math.floor(Math.random() * 100) + 1
+let resultMessage = ''
+let history = []
+let numAvailableGuesses = 5;
+let count = 0;
+console.log("This is what the computer chose:", randomNum)
+
+function guess() {
+
+    let userNum = document.getElementById("guessNumber").value
+    count += 1;
+    if (randomNum > userNum) {
+        resultMessage = userNum+" is too low."
+        numAvailableGuesses = numAvailableGuesses - 1
+    } else if (randomNum < userNum) {
+        resultMessage = userNum+" is too high."
+        numAvailableGuesses = numAvailableGuesses - 1
+    } else if (randomNum = userNum) {
+        resultMessage = userNum+" is correct!"
+        numAvailableGuesses = numAvailableGuesses - 1 
+        document.getElementById("guessButton").disabled = true;
+    } 
+
+    if (count >= 5) {
+        document.getElementById("guessButton").disabled = true;
+    }
+    history.push(userNum)
+    document.getElementById("resultArea").innerHTML = `${resultMessage}`
+    document.getElementById("historyArea").innerHTML = `History: ${history}`
+    document.getElementById("remainingGuesses").innerHTML = `Remaining Guesses: ${numAvailableGuesses}`
+}
+
+function reset() {
+    location.reload();
+}
