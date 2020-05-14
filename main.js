3172cacafc128e42b69617a078a58cdb14ebc3aa
alyssabao <64719777+alyssabao@users.noreply.github.com>
2020-05-14 08:07:01 +0700

    finished required features

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ let resultMessage = ''
 let history = []
 let numAvailableGuesses = 5;
 let count = 0;
+let element = document.getElementById("design")
 console.log("This is what the computer chose:", randomNum)
 
 function guess() {
-
     let userNum = document.getElementById("guessNumber").value
     count += 1;
     if (randomNum > userNum) {
@@ -19,11 +19,14 @@ function guess() {
         resultMessage = userNum+" is correct!"
         numAvailableGuesses = numAvailableGuesses - 1 
         document.getElementById("guessButton").disabled = true;
+        element.style.backgroundColor = "#5db410";
     } 
-
     if (count >= 5) {
         document.getElementById("guessButton").disabled = true;
+        resultMessage = "Oops, you lost :("
+        element.style.backgroundColor = "#b43610";
     }
+    $('#guessNumber').val('');
     history.push(userNum)
     document.getElementById("resultArea").innerHTML = `${resultMessage}`
     document.getElementById("historyArea").innerHTML = `History: ${history}`
